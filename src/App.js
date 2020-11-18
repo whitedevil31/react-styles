36610ab5fc228310d74efe52424166fb1cac6325
whitedevil31 <magz3116@gmail.com>
2020-11-18 23:44:04 +0530

    init commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Button } from "@material-ui/core";
+import "./App.css";
+import logo from "./logo.jpg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="header">
+        <div className="header-logo">
+          <img className="logo" src={logo} />
+        </div>
+
+        <div className="header-left">
+          <a target="_blank" href="https://www.w3schools.com">
+            CONTACT US{" "}
+          </a>
+          <Button variant="contained" color="secondary">
+            REGISTER
+          </Button>
+          <Button variant="contained" color="secondary">
+            ABOUT US
+          </Button>
+        </div>
+      </div>
     </div>
   );
 }
