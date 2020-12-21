ca775e22a41e8250ecb1e73ae3c318449ae64223
whitedevil31 <magz3116@gmail.com>
2020-12-21 19:23:54 +0530

    firebase testing #1

--- src/App.js
+++ src/App.js
@@ -1,41 +1,27 @@
-import React from "react";
-import { Button } from "@material-ui/core";
+import React, { useState } from "react";
+import { Button, Menu, MenuItem } from "@material-ui/core";
 import "./App.css";
 import logo from "./logo.jpg";
-import { Dropdown } from "react-bootstrap";
-import "bootstrap/dist/css/bootstrap.min.css";
+import market from "./market.svg";
 
 function App() {
+  // const [anchorEl, setAnchorEl] = useState(null);
+  // const handleClick = (event) => {
+  //   setAnchorEl(event.currentTarget);
+  // };
+
+  // const handleClose = () => {
+  //   setAnchorEl(null);
+  // };
+
+  // const obl = { name: "my name is nanda" };
+
+  // const [click, setClick] = useState("");
+  // console.log(click);
   return (
-    <div className="container">
-      <div className="header">
-        <div className="header-logo">
-          <img className="logo" src={logo} />
-        </div>
-
-        <div className="header-left">
-          <a target="_blank" href="https://www.w3schools.com">
-            CONTACT US{" "}
-          </a>
-          <Button variant="contained" color="secondary">
-            REGISTER
-          </Button>
-          <Button variant="contained" color="secondary">
-            ABOUT US
-          </Button>
-          <Dropdown>
-            <Dropdown.Toggle variant="success" id="dropdown-basic">
-              Dropdown Button
-            </Dropdown.Toggle>
-
-            <Dropdown.Menu>
-              <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-              <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-              <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-            </Dropdown.Menu>
-          </Dropdown>
-        </div>
-      </div>
+    <div className="container-js">
+      <h1>parent</h1>
+      <h1>child</h1>
     </div>
   );
 }
